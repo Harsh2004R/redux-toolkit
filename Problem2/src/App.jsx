515e6173da131211b1efc217962c7c2bf99bc0b8
Harsh2004R <harshsharmaktm03@gmail.com>
2026-04-22 19:57:26 +0530

    Practicing new problem

---
+++ Problem2/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Box } from "@chakra-ui/react";
+function App() {
+  return (
+    <Box border="1px solid red" w="100%" h="100vh">
+
+    </Box>
+  )
+}
+
+export default App
